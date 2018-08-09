37325920b106c9722a3ef7a34453090afb799d7e
Said Gourida <33969482+Said113@users.noreply.github.com>
2018-08-09 20:34:30 +0200

    change object

--- Text Editor/main.py
+++ Text Editor/main.py
@@ -3,22 +3,22 @@ from tkinter import *
 root=Tk()
 menu=Menu(root)
 root.config(menu=menu)
-submenu=Menu(menu,tearoff=0)
-menu.add_cascade(label="File",menu=submenu)
-submenu.add_command(label="New")
-submenu.add_command(label="Open")
-submenu.add_command(label="Close")
+first=Menu(menu,tearoff=0)
+menu.add_cascade(label="File",menu=first)
+first.add_command(label="New")
+first.add_command(label="Open")
+first.add_command(label="Close")
 
 #just make separator between the labels
-submenu.add_separator()
+first.add_separator()
 
-submenu.add_command(label="Sava")
-submenu.add_command(label="Sava As")
+first.add_command(label="Sava")
+first.add_command(label="Sava As")
 
 #just make separator between the labels
-submenu.add_separator()
+first.add_separator()
 
-submenu.add_command(label="Page Setup")
-submenu.add_command(label="Quit")
+first.add_command(label="Page Setup")
+first.add_command(label="Quit")
 
 root.mainloop()
