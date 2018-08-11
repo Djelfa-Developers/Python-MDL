dd0213fe7fd9f6f58e4e619f0d29cfb33c553189
Said Gourida <33969482+Said113@users.noreply.github.com>
2018-08-11 22:31:32 +0200

    Update functions.py

--- Text Editor/functions.py
+++ Text Editor/functions.py
@@ -2,12 +2,15 @@
 # -*- coding: utf-8 -*-
 # Said Gourida
 
+
 import sys
 
 if sys.version_info[0] == 3:  # Just checking your Python version to import Tkinter properly.
     from tkinter import *
+    from tkinter import messagebox
 else:
-    from Tkinter import *
+    from Tkinter import * # this for python 2 
+    import tkMessageBox # this for python2.x when you want to import messagebox
 
     
     
