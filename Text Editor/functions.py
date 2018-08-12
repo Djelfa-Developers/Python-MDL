9642f13449b42730244bdb1cf3204bb51731156b
Said Gourida <33969482+Said113@users.noreply.github.com>
2018-08-12 23:17:12 +0200

    add About window

--- Text Editor/functions.py
+++ Text Editor/functions.py
@@ -12,8 +12,18 @@ else:
     from Tkinter import * # this for python 2 
     import tkMessageBox # this for python2.x when you want to import messagebox
 
+
+
+def about():
+    root_1 =Tk()
+    #root.withdraw()
+    root_1.title("Moino Abont")
+    root_1.resizable(False,False)
+    root_1.geometry("400x400+400+170")
+    root_1.config(background="#593489")
     
-    
-def quit():
-    root.destroy()
+    def close():
+        root_1.destroy()    
+        
+    lb=Button(root_1,text="Close",width=50,command=close).place(x=22,y=100)
 
