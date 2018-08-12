6289f9d1eb27449e654013b8287b18ac152b52c1
Said Gourida <33969482+Said113@users.noreply.github.com>
2018-08-13 00:49:54 +0200

    Update functions.py

--- Text Editor/functions.py
+++ Text Editor/functions.py
@@ -24,6 +24,9 @@ def about():
     
     def close():
         root_1.destroy()    
-        
+    
+    #style=ttk.Style()
+    #style.configure("TButton")
+    #lb=ttk.Button(root_1,text="Close",command=close).place(x=22,y=350,width=360)
     lb=Button(root_1,text="Close",width=50,foreground="white",borderwidth=2,width=50,bg="#F88C39",command=close).place(x=22,y=350)
 
