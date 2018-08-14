f4e44f487a5e0799d4a6c6ebd5c80a1f0cef5692
Said Gourida <33969482+Said113@users.noreply.github.com>
2018-08-14 20:00:27 +0200

    Update functions.py

--- Text Editor/functions.py
+++ Text Editor/functions.py
@@ -23,19 +23,18 @@ def about():
     root_1.geometry("400x400+400+170")
     root_1.config(background="#593489")
     
-    
+    def close():
+        root_1.destroy() 
     
     #style=ttk.Style()
     #style.configure("TButton")
     #lb=ttk.Button(root_1,text="Close",command=close).place(x=22,y=350,width=360)
-    lb=Button(root_1,text="Close",width=50,foreground="white",borderwidth=2,width=50,bg="#F88C39",command=close).place(x=22,y=350)
+    lb=Button(root_1,text="Close",width=50,foreground="white",borderwidth=2,bg="#F88C39",command=close).place(x=22,y=350)
    
 
 
 
-#def close():
-
-    #root_1.destroy()    
+   
     
     
 
