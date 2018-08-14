c95f8a78fa233b77d5f6799008b22d2e5010ec82
Said Gourida <33969482+Said113@users.noreply.github.com>
2018-08-15 01:05:26 +0200

    Update functions.py

--- Text Editor/functions.py
+++ Text Editor/functions.py
@@ -54,6 +54,12 @@ def about():
     release1.place(x=192,y=172,width=100)
     
     
+    owner1=Text(myframe,fg="white",font="none 10 bold",bg="#303030",borderwidth=0,height=0)
+    owner1.insert(0.0,"Gourida Said")
+    owner1.configure(state="disabled")
+    owner1.place(x=192,y=192,width=100)
+    
+    
     #style=ttk.Style()
     #style.configure("TButton")
     #lb=ttk.Button(root_1,text="Close",command=close).place(x=22,y=350,width=360)
